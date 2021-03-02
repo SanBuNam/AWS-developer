2332670f220694907cb58544a55d3972acc1169e
David Lee <david.lee@corp.nmsigroup.com>
2021-03-02 13:50:29 -0800

    Creating a CloudFront Distribution

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://s3.amazonaws.com/hamster-bucket-david/'
+const base = 'https://dr9rb3ildwj8z.cloudfront.net'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
