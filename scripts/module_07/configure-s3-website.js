1df30a49d545d4b80ff39af43254f21749801b09
David Lee <david.lee@corp.nmsigroup.com>
2021-03-02 14:11:11 -0800

    Configuring an S3 Static Websites

--- scripts/module_07/configure-s3-website.js
+++ scripts/module_07/configure-s3-website.js
@@ -1,18 +1,24 @@
-// Imports
 const AWS = require('aws-sdk')
-
-AWS.config.update({ region: '/* TODO: Add your region */' })
-
-// Declare local variables
+AWS.config.update({ region: 'us-west-2' })
 const s3 = new AWS.S3()
 
 configureS3Site('/* TODO: Add your S3 bucket name */')
 .then(data => console.log(data))
 
 function configureS3Site (bucketName) {
-  // TODO: Create params const object
+  const params = {
+    Bucket: bucketName,
+    WebsiteConfiguration: {
+      IndexDocument: {
+        Suffix: 'index.html'
+      }
+    }
+  }
 
   return new Promise((resolve, reject) => {
-    // Call putBucketWebsite
+    s3.putBucketWebsite(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
